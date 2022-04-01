13ea27217bcbcbf4e19afc557f2a5cd0fa6e7e43
Jheyson Herrera <herrerajheyson@gmail.com>
2022-04-01 17:37:52 -0500

    Instalación de vee-validate

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import App from "./App.vue";
 
 import router from "./routes/router";
 
+import { configure } from "vee-validate";
+
+configure({
+  validateOnBlur: true,
+  validateOnChange: true,
+  validateOnInput: false,
+  validateOnModelUpdate: true,
+});
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 createApp(App).use(router).use(store).mount('#app')
